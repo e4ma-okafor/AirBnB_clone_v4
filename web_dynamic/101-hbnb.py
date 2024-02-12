97376697dd962abd499dd0ae11f063ada21a75b7
e4ma-okafor <ifeomasandra13@gmail.com>
2024-02-12 05:11:21 +0000

    reviews

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -4,7 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-import uuid
+import uuid;
 
 # flask setup
 app = Flask(__name__)
@@ -23,7 +23,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/0-hbnb/')
+@app.route('/101-hbnb')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
@@ -34,14 +34,12 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    cache_id = (str(uuid.uuid4()))
-    return render_template('1-hbnb.html',
-                           states=states,
+    return render_template('101-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=state_objs,
                            amens=amens,
                            places=places,
-                           users=users,
-                           cache_id=cache_id)
-
+                           users=users)
 
 if __name__ == "__main__":
     """
